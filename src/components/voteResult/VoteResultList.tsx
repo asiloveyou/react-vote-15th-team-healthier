ce9a2d4f7e06a42310ff8ab915e8f6dbeb878b48
juhyojeong <juhyojeong58@gmail.com>
2022-06-24 12:49:52 +0900

    chore: result 애니메이션

--- src/components/voteResult/VoteResultList.tsx
+++ src/components/voteResult/VoteResultList.tsx
@@ -1,8 +1,20 @@
 import React from "react";
 import { IVoteList } from "../../config/interface";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
-const VoteItem = styled.section`
+const VoteResultAnimation = keyframes`
+  0% {
+    clip-path: polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%);
+    transform: translateY(100%);
+  }
+  95% {
+      transform: translateY(0%);
+  }
+  100% {
+    clip-path: polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%);
+  }
+`;
+const VoteItem = styled.section<{ delay: number }>`
   display: flex;
   justify-content: space-between;
 
@@ -15,6 +27,9 @@ const VoteItem = styled.section`
 
   border-bottom: 0.01rem solid ${({ theme }) => theme.gray};
   color: ${({ theme }) => theme.gray};
+
+  animation: ${VoteResultAnimation} 1s ease-in-out both;
+  animation-delay: ${({ delay }) => delay}s;
 `;
 const VoteName = styled.section`
   padding-left: 0.5rem;
@@ -27,8 +42,8 @@ const VoteNumber = styled.section`
 const VoteResultList = ({ partList }: IVoteList) => {
   return (
     <>
-      {partList.map((part) => (
-        <VoteItem key={part.id}>
+      {partList.map((part, idx) => (
+        <VoteItem key={part.id} delay={idx * 0.4}>
           <VoteName>{part.name}</VoteName>
           <VoteNumber>123표</VoteNumber>
         </VoteItem>
