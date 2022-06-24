02434238c7f2076f0bede430b98a764baa4f9960
asiloveyou <75231844+asiloveyou@users.noreply.github.com>
2022-06-24 19:02:03 -0400

    chore: 불필요한 코드 삭제

--- src/lib/post.js
+++ src/lib/post.js
@@ -3,14 +3,6 @@ import axios from "axios";
 // promise 요청 타임아웃 시간 선언
 const TIME_OUT = 300 * 1000;
 
-// 에러 처리를 위한 status 선언
-const statusError = {
-  status: false,
-  json: {
-    error: ["연결이 원활하지 않습니다. 잠시 후 다시 시도해 주세요"],
-  },
-};
-
 // 백으로 요청할 promise
 const requestPromise = (link, credentials) => {
   return new Promise((resolve, reject) => {
